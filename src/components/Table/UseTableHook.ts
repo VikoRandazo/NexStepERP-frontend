a75b265ca5da90608dd0e64db4deb787b6ce24ea
Viko Randazo <vikorandazo@gmail.com>
2023-09-23 12:34:21 +0300

    checkbox selectAll table functionality fixed

--- src/components/Table/UseTableHook.ts
+++ src/components/Table/UseTableHook.ts
@@ -1,21 +1,28 @@
 import { useState } from "react";
 
 export const useTableHook = (data: any[]) => {
-  const [hiddenColumns, setHiddenColumns] = useState<string[]>([`_id`, `imageUrl`, `category`, `description`]);
+  const [hiddenColumns, setHiddenColumns] = useState<string[]>([
+    `_id`,
+    `imageUrl`,
+    `category`,
+    `description`,
+  ]);
   const [selectAll, setSelectAll] = useState<boolean>(false);
 
   const prepareTableData = () => {
     if (data) {
-      const items = data.map((item) => {
-        return Object.entries(item)
-          .filter(([key]) => !hiddenColumns.includes(key))
-          .reduce((acc: any, [key, value]) => {
-            acc[key] = value;
-            return acc;
-          }, {});
-      });
-      return items;
-    }
+        const items = data.map((item, i) => {
+          const processedItem = Object.entries(item)
+            .filter(([key]) => !hiddenColumns.includes(key))
+            .reduce((acc: any, [key, value]) => {
+              acc[key] = value;
+              return acc;
+            }, {});
+          
+          return { Row: [i + 1], ...processedItem };
+        });
+        return items;
+      }
   };
 
   const prepareTableColumns = () => {
@@ -24,9 +31,10 @@ export const useTableHook = (data: any[]) => {
       const itemKeys = Object.entries(item);
       const mappedKeys = itemKeys.map(([key]) => key);
       const filterKeys = mappedKeys.filter((key) => !hiddenColumns.includes(key));
+      const addSerialNum = filterKeys.unshift(`Row`);
       return filterKeys;
     }
   };
 
-  return { selectAll,setSelectAll, hiddenColumns, prepareTableData, prepareTableColumns };
+  return { selectAll, setSelectAll, hiddenColumns, prepareTableData, prepareTableColumns };
 };
